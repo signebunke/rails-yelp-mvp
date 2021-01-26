b14e06b92256a1ded858769c48e872a14a7d84a8
Signe Bunke <signebunke@gmail.com>
2021-01-26 22:34:36 +0100

    seeds almost done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+puts 'Database is clean'
+
+puts 'Creating restaurants...'
+5.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_addres,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: rand('chinese', 'italian', 'japanese', 'french', 'belgian')
+  )
+  puts "restaurant #{restaurant.name} is created"
+end
+
+puts 'Finished!'
